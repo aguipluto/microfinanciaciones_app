a20fafa627c6e3343aba9653748442a6a8a49f73
aguipluto <aguipluto@gmail.com>
2013-08-12 13:20:39 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MicrofinanciacionesApp::Application.config.secret_key_base = '9ad115e19aeb8ae4362db19f560fb60902353408d0d454bc82001aaa5ed7c5653b8e5ff578c40e4dd7fe6e50347f400075ddee07abac2a115d65550f4a19f2cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MicrofinanciacionesApp::Application.config.session_store :cookie_store, key: '_microfinanciaciones_app_session'
