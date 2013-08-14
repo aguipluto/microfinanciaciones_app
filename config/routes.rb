3694fb56a4f37b0de6eb4c3acdcf6de26db45760
aguipluto <aguipluto@gmail.com>
2013-08-14 10:51:01 +0200

    Layout and routes finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 MicrofinanciacionesApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -43,7 +46,7 @@ MicrofinanciacionesApp::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,44 +3,37 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "MicroFinanciaciones CEU" }
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
+    it { should have_content('MicroFinanciaciones CEU') }
+    it { should have_title(full_title) }
+    it { should_not have_title('| Home') }
 
-    it "should have the content 'Microfinanciaciones CEU'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('MicroFinanciaciones CEU')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      expect(page).to have_title("MicroFinanciaciones CEU | Home")
-    end
   end
+
 
   describe "Help page" do
+    before { visit help_path }
+    it { should have_content('Ayuda') }
+    it { should have_title(full_title('Ayuda')) }
+  end
 
-    it "should have the content 'Ayuda'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Ayuda')
-    end
+  describe " About page " do
+    before { visit about_path }
 
-    it "should have the title 'Ayuda'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("MicroFinanciaciones CEU | Ayuda")
-    end
+    it { should have_content('¿Quiénes somos?') }
+    it { should have_title(full_title('¿Quiénes somos?')) }
 
   end
 
-  describe "About page" do
+  describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content '¿Quiénes somos?'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('¿Quiénes somos?')
-    end
-
-    it "should have the title '¿Quiénes somos?'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("MicroFinanciaciones CEU | ¿Quiénes somos?")
-    end
+    it { should have_content('Contacte con nosotros') }
+    it { should have_title(full_title('Contacto')) }
   end
 
 end
