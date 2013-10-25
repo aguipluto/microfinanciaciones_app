dc75169cabf923120bf25b23108572d3f40369ef
aguipluto <aguipluto@gmail.com>
2013-10-25 19:27:58 +0200

    SSL en produccion

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ MicrofinanciacionesApp::Application.routes.draw do
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  root 'static_pages#home'
+  root to: 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
