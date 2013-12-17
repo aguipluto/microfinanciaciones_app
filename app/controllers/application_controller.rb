da561291ca49413fd873659a3892da36b9af45f8
aguipluto <aguipluto@gmail.com>
2013-12-17 19:21:22 +0100

    error management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,16 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   http_basic_authenticate_with :name => 'teo', :password => 'teo' if Rails.env.production?
 
+  def render_not_found(exception)
+    render :template => "/errors/404.html.erb",
+           :layout => 'errors.html.erb'
+  end
+
+  def render_error(exception)
+    ExceptionNotifier::Notifier
+    .exception_notification(request.env, exception)
+    .deliver
+    render :template => "/errors/500.html.erb",
+           :layout => 'errors.html.erb'
+  end
 end
