d933c2cc2b22bd919b77b328397a5e6645d28a8e
Antonio <aguipluto@gmail.com>
2014-04-25 18:44:39 +0200

    sendgrid for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,12 +115,12 @@ MicrofinanciacionesApp::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-      address: 'smtp.gmail.com',
+      address: 'smtp.sendgrid.net',
       port: 587,
-      domain: 'microfinanciacionesceu.herokuapp.com',
-      user_name: 'aguipluto@gmail.com',
-      password: ENV["GMAIL_PASSWORD"],
-      authentication: 'plain',
+      domain: 'heroku.com',
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      authentication: :plain,
       enable_starttls_auto: true
   }
 
