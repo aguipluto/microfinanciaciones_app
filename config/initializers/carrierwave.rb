279e4f7b0900e1bfdb4de31d8d58245936ebbbe8
Antonio <aguipluto@gmail.com>
2014-06-09 18:14:47 +0200

    Aws st repair

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AKIAJCQQWYW6MBDA2AXA',                        # required
-      :aws_secret_access_key  => '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',                        # required
+      :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']                        # required
   }
-  config.fog_directory  = 'microfinanciacionesapp-images'                     # required
+  config.fog_directory  = ENV['AWS_BUCKET']                    # required
 
 end
--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AKIAJCQQWYW6MBDA2AXA',                        # required
-      :aws_secret_access_key  => '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',                        # required
+      :aws_access_key_id      => ENV['AWS_ACC'],                        # required
+      :aws_secret_access_key  => ENV['AWS_SEC'],                        # required
   }
   config.fog_directory  = 'name_of_bucket'                     # required
 end
